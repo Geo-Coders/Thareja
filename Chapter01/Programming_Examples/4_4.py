5f463483db84ad8ec6d396580bd4de45c2e8f7be
Eunice94 <eunicewachira94@gmail.com>
2022-01-14 17:23:10 +0300

    added

---
+++ Chapter01/Programming_Examples/4_4.py
@@ -0,0 +1,9 @@
+#----- program 4.4 -----
+# Write a program to find larger of two numbers
+a = int(input('Enter the value of a : '))
+b = int(input('Enter the value of b : '))
+if a > b:
+    large = a
+else:
+    large = b
+print('Large = ',large )
