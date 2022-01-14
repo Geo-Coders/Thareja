2e7323f6f275e8827066f28cbedd4427133cdda2
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-14 19:21:10 +0300

    Update 4_10.py
    Updated the use of f-string

--- Chapter01/Programming_Examples/4_10.py
+++ Chapter01/Programming_Examples/4_10.py
@@ -5,11 +5,11 @@ num2 = int(input('Enter the second number : '))
 num3 = int(input('Enter the third number : '))
 if num1 > num2:
     if num1 > num3:
-        print(num1,'is greater than', num2, 'and', num3)
+        print(f'{num1} is greater than {num2} and {num3}')
     else:
-        print(num3,'is greater than', num1, 'and', num2)
+        print(f'{num3} is greater than {num1} and {num2}')
 elif num2 > num3:
-     print(num2,'is greater than', num1, 'and', num3)
+     print(f'{num2} is greater than {num1} and {num3}')
 else:
     print('The three numbers are equal')
         
