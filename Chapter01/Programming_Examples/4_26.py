ec8e24abb8544d491cd0c86abf5eab27f68cca1b
Eunice94 <eunicewachira94@gmail.com>
2022-01-14 20:39:32 +0300

    update

--- Chapter01/Programming_Examples/4_26.py
+++ Chapter01/Programming_Examples/4_26.py
@@ -1,4 +1,5 @@
 #------- program 4.26 ------
 # Write a program to calculate GCD of two numbers
 
-num1 = 
+num1 = int(input('Enter the two numbers : '))
+num1 = int(input('Enter the two numbers : '))
