bcfd46121f94f4bc2de668b01bb79223e7bd5477
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-14 19:38:16 +0300

    updated

--- Chapter01/Programming_Examples/4_24.py
+++ Chapter01/Programming_Examples/4_24.py
@@ -1,24 +1,19 @@
 #------ program 4.24 -----
-# Write a program to read a character untill a * is encountered. Also count the number of uppercase, lowercase, and numbers entered by the users
+# Write a program to read a character until a * is encountered. Also count the number of uppercase, lowercase, and numbers entered by the users
 
-ch = input('Enter any character : ')
 num_count = 0
 up_count = 0
 low_count = 0
-if ch >= '0' and ch <= '9':
-    num += 1
-elif ch>= 'a' and ch <= 'z':
-    low_count += 1
-elif ch >= 'A' and ch <= 'Z':
-    up_count += 1
+
 while ch != '*':
     ch = input('Enter any character : ')
-    if ch >= '0' and ch <= '9':
+    if ch.isdigit():
         num_count += 1
-    elif ch >='a' and ch <= 'z':
-        low_count +=1
-    elif ch >= 'A' and ch <= 'Z':
+    elif ch.islower():
+        low_count += 1
+    elif ch.isupper():
         up_count += 1
+
 print(f'Number of lowercase character are : {low_count}')
 print(f'Number of uppercase character are : {up_count}')
 print(f'Number of numerals are : {num_count}')
