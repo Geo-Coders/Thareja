2df2d70d486e8762d20b9dbab0ae408c3f6a8486
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-14 17:59:01 +0300

    Update and rename 4._6.py to 4._06.py
    Added the use of f-string

---
+++ Chapter01/Programming_Examples/4._06.py
@@ -0,0 +1,9 @@
+#----- program 4.6 -----
+# Write a program to enter any character. if the entered character is in lowercase then convert it into uppercase and if it is an uppercase, the  convert it into lowercase
+ch = input('Enter any character : ')
+if ch.isupper():
+    ch = ch.lower()
+    print('The entered character was in uppercase. In lowercase it is : {ch}')
+else:
+    ch = ch.upper()
+    print('The entered character was in lowercase. In upperrcase it is : {ch}')
