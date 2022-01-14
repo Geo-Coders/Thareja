175cc8a4ac939c72ba5baf0300cca7c7ad5e7bbb
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-14 13:26:53 +0300

    updated Co-authored-by: Eunice94 <Eunice94@users.noreply.github.com>

--- Chapter01/Examples/Tests.py
+++
@@ -1,2 +0,0 @@
-# Here is where we code all our Test functions and Classes
-# welcome to python programming
---
+++ Chapter01/Programming_Examples/4_1.py
@@ -0,0 +1,7 @@
+
+# ----- Program 4.1 ------
+# a program to determine whether one is eligible to vote
+
+age = int(input('Enter the age :'))
+if(age >= 18):
+    print('You are eligible to vote')
