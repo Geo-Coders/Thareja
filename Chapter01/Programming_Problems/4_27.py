0e312fc6ad1af923cf2a23a2b29556a4b1dea1d6
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-15 17:01:38 +0300

    updated

--- Chapter01/Programming_Problems/4_27.py
+++ Chapter01/Programming_Problems/4_27.py
@@ -3,32 +3,32 @@
 print('--- A Program to display the quadrant of an angle from the user ---')
 print()
 
-prompt_angle = int(input("Enter an Angle between 0 - 360, or press -1 to quit : "))
+prompt_angle = int(input('Enter an Angle between 0 - 360, or press -1 to quit : '))
 while True:
     flag = 0
     if prompt_angle == -1:
         flag = 1
         print()
-        print("Thank you for trying, See you Tomorrow!")
+        print('Thank you for trying, See you Tomorrow!')
         break
     if flag == 0:
         if prompt_angle >= 0 and prompt_angle <= 90:
-            print(f" The Angle {prompt_angle} is in First Quadrant.")
+            print(f' The Angle {prompt_angle} is in First Quadrant.')
 
         elif prompt_angle > 90 and prompt_angle <= 180:
-            print(f" The Angle {prompt_angle} is in Second Quadrant.")
+            print(f' The Angle {prompt_angle} is in Second Quadrant.')
 
         elif prompt_angle > 180 and prompt_angle <= 270:
-            print(f" The Angle {prompt_angle} is in Third Quadrant.")
+            print(f' The Angle {prompt_angle} is in Third Quadrant.')
 
         elif prompt_angle > 270  and prompt_angle <= 360:
-            print(f" The Angle {prompt_angle} is in Forth Quadrant.")
+            print(f' The Angle {prompt_angle} is in Forth Quadrant.')
 
         else:
             print('Enter a valid angle')
 
     print()
-    prompt_angle= int(input(" Enter another Angle between 0 - 360: "))
+    prompt_angle= int(input(' Enter another Angle between 0 - 360: '))
     
 
 
--- Chapter01/Programming_Problems/4_31.py
+++ Chapter01/Programming_Problems/4_31.py
@@ -6,9 +6,9 @@
 # Finally, calculate the charges based on following rules and 
 # display the result on the screen.
 
-print (" *****\tA PROGRAM TO CALCULATE PARKING CHARGES OF A VEHICLE\t***** ")
+print (' *****\tA PROGRAM TO CALCULATE PARKING CHARGES OF A VEHICLE\t***** ')
 print ('*' * 100)
-print ("\tChoose the type of a vehicle you have by pressing the corresponding number.")
+print ('\tChoose the type of a vehicle you have by pressing the corresponding number.')
 
 cond = True
 type = 0
@@ -18,8 +18,8 @@ while cond:
     if type in [1,2,3]:
         cond = False
     if type > 3:
-        print ("The type of vehicle selected is not supported")
-        choice = input ("\nWould you wish to continue? (y/n) \n\t>>> ")
+        print ('The type of vehicle selected is not supported')
+        choice = input ('\nWould you wish to continue? (y/n) \n\t>>> ')
        
         if choice.lower() != 'y':
             flag = 1 
@@ -29,33 +29,33 @@ while cond:
     else:
        
         if type not in [1,2,3]:
-            type = int (input ("\t1.cars \n\t2.Truck/bus. \n\t3.Scooter/Cycle/Motor cycle.\n>>> "))
+            type = int (input ('\t1.cars \n\t2.Truck/bus. \n\t3.Scooter/Cycle/Motor cycle.\n>>> '))
         
 else:
-    in_hrs = int(input("Enter the time of entrace (HH) : "))
-    in_min = int(input("Enter the time of entrace (MM) : "))
-    out_hrs = int(input("Enter the time of Leaving (HH) : "))
-    out_min = int(input("Enter the time of leaving (MM) : "))
+    in_hrs = int(input('Enter the time of entrace (HH) : '))
+    in_min = int(input('Enter the time of entrace (MM) : '))
+    out_hrs = int(input('Enter the time of Leaving (HH) : '))
+    out_min = int(input('Enter the time of leaving (MM) : '))
 
     time_spent = (out_hrs + (out_min/60)) - (in_hrs + (in_min/60))
 
     if time_spent <= 3:
             if flag == 0:
                 if type == 1:
-                    print(f"Your charges are : {time_spent * 10} £.")
+                    print(f'Your charges are : {time_spent * 10} £.')
                 elif type == 2:
-                    print(f"Your charges are : {time_spent * 20} £.")
+                    print(f'Your charges are : {time_spent * 20} £.')
                 elif type == 3:
-                    print(f"Your charges are : {time_spent * 5} £.")
+                    print(f'Your charges are : {time_spent * 5} £.')
     elif time_spent > 3:
             if type == 1:
-                print(f"Your charges are : {time_spent * 20} £.")
+                print(f'Your charges are : {time_spent * 20} £.')
             elif type == 2:
-                print(f"Your charges are : {time_spent * 30} £.")
+                print(f'Your charges are : {time_spent * 30} £.')
             elif type == 3:
-                print(f"Your charges are : {time_spent * 10} £.")
+                print(f'Your charges are : {time_spent * 10} £.')
     else:
-        print("The type of Vehicyle selected is not supported")
+        print('The type of Vehicyle selected is not supported')
 
 
 print('*' * 100)
