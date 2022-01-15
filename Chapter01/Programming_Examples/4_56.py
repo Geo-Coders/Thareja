c17b109af67605a3532d94f203d58f76808b529a
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-15 10:07:03 +0300

    added a space

--- Chapter01/Programming_Examples/4_56.py
+++ Chapter01/Programming_Examples/4_56.py
@@ -1,4 +1,4 @@
 # Using a for loop, write a program that prints out the decimal equivalents of 1/2, 1/3, 1/4,...,1/10
 
 for i in range(1, 10):
-    print('1/', i, ' =%f' %(1.0/i))
+    print('1/', i, ' = %f' %(1.0/i))
