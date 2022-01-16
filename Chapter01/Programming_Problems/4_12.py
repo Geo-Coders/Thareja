6db3cf9a5d501400a3ab297be0cb18ee08f9c463
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-01-16 21:38:47 +0300

    check the logic of this code

--- Chapter01/Programming_Problems/4_12.py
+++ Chapter01/Programming_Problems/4_12.py
@@ -1,11 +1,12 @@
-# Write a program that prints anumber, it's square, and cube repeatedly in the range (1,n)
+# Write a program that prints a number, it's square, and cube repeatedly in the range (1,n)
 
 n = int(input('Enter a number : '))
 
 for i in range (1, n+1):
     sqr = i**2
     cube = i**3
-    
-print(f'The square of {n} is {sqr}')
-print(f'The cube of {n} is {cube}')
+
+    print(f'The square of {i} is {sqr}')
+    print(f'The cube of {i} is {cube}')
+    print()
     
