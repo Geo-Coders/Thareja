bca49c9c3a1193573636158d92e937e72128d17f
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-02-02 21:29:03 +0300

    added

---
+++ Chapter09/Programming_Problems/9_01.py
@@ -0,0 +1,9 @@
+# Write a program that has a class Point with attributes as the X and Y
+# co-ordinates. Make two objects of this class and find the midpoint of 
+# both the points
+
+print('--- A Program to find the mid of two points ---')
+print()
+
+
+
