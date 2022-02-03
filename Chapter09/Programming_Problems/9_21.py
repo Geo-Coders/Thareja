5f786ca5d770f1df23b79c93adb0e51450d803cc
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-02-03 19:46:09 +0300

    updated

--- Chapter09/Programming_Problems/9_21.py
+++ Chapter09/Programming_Problems/9_21.py
@@ -7,45 +7,7 @@ print()
 import numpy
 
 class Matrix:
-    def main():
-        while True:
-            print('''
-                1. Read two Matrices
-                2. Add
-                3. Subtract
-                4. Multiply
-                5. Divide
-                6. Transpose
-                7. Quit
-            ''')
-            try:
-                while True:
-                    choice_= int(input('Enter your choice from the given choices : '))
-                    if choice_ not in range(1,8):
-                        choice_= int(input('Please choose from the given choices : '))
-                    else:
-                        break
-
-                if choice_ == 1:
-                    _read()
-                elif choice_ == 2:
-                    print('Addition')
-                elif choice_ == 3:
-                    print('Subtract')
-                elif choice_ == 4:
-                    print('Multiply')
-                elif choice_ == 5:
-                    print('Divide')
-                elif choice_ == 6:
-                    print('Transpose')
-                else:
-                    break
-
-
-            except Exception as e:
-                print(e)
-
-    def _read(self):
+    def read(self):
         while True:
             var1= int(input('Enter the first number of the Matrix : '))
             var2= int(input('Enter the second number of the Matrix : '))
@@ -63,7 +25,49 @@ class Matrix:
                 ([[var5,var6], [var7,var8]])
             )
             print('Confirm your inputs')
-            print(f'x = {self._x} \t y = {self._y}')
+            print(f'x = \n{self._x}\ny = \n{self._y}')
+            choice_= input('Enter y/n : ')
+            if choice_ == 'n':
+                break
+
+    def main(self):
+        while True:
+            print('''
+                1. Read two Matrices
+                2. Add
+                3. Subtract
+                4. Multiply
+                5. Divide
+                6. Transpose
+                7. Quit
+            ''')
+            try:
+                choice_= int(input('Enter your choice from the given choices : '))
+                while True:
+                    if choice_ not in range(1,8):
+                        choice_= int(input('Please choose from the given choices : '))
+                        print()
+                    else:
+                        break
+
+                if choice_ == 1:
+                    self.read()
+                elif choice_ == 2:
+                    print('Addition')
+                elif choice_ == 3:
+                    print('Subtract')
+                elif choice_ == 4:
+                    print('Multiply')
+                elif choice_ == 5:
+                    print('Divide')
+                elif choice_ == 6:
+                    print('Transpose')
+                else:
+                    break
+
+
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
-    Matrix.main()
+    Matrix().main()
