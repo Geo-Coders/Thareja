002224464ef6dc7718c839e2a750052084cc602f
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2022-02-10 18:13:34 +0300

    added

--- Chapter07/Programming_Exercises/7_01.py
+++ Chapter07/Programming_Exercises/7_01.py
@@ -1,1 +1,12 @@
-# Place Holder
+# Write a program that reads text from a file and writes it into another file but in the reverse order
+
+print('--- A Program to Reverse texts ---')
+print()
+
+file = "<path to file>"
+with open(file) as f:
+    lines = f.readlines()
+
+reverse_file = "e:\\python\\reversed.txt"
+with open(reverse_file, 'w') as rev:
+    rev.writelines(lines[::-1])
